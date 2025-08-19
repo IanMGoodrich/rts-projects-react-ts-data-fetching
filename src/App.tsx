b9a08aab260370fbe7fdd9795118ea7073874978
Ian Goodrich <ian.m.goodrich.actual@gmail.com>
2025-08-19 13:18:49 -0700

    feat(app): Update dapp component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,65 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { get } from './utilities/http';
+import { useState, useEffect, type ReactNode } from 'react';
+import BlogPosts, { type BlogPost } from './components/BlogPosts';
+import dataFetching from './assets/data-fetching.png';
+import ErrorMessage from './components/ErrorMessage';
+
+type RawDataBlogPost = {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [fetchedPosts, setFetchPosts] = useState<BlogPost[]>();
+  const [isFetching, setIsFetching] = useState<boolean>(false);
+  const [fetchError, setFetchError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchPostsData = async () => {
+      setIsFetching(true);
+      try {
+      const data = await get('https://jsonplaceholder.typicode.com/posts') as RawDataBlogPost[];
+
+      const blogPostsData: BlogPost[] = data.map((post) => ({
+        id: post.id,
+        title: post.title,
+        text: post.body,
+      }));
+      setFetchPosts(blogPostsData);
+      } catch (error) {
+        if (error instanceof Error) {
+          setFetchError(error.message);
+        } else {
+          setFetchError('An unknown error occurred');
+        }
+      }
+      setIsFetching(false);
+    }
+    fetchPostsData();
+  }, [])
+
+  let content: ReactNode;
+
+
+  if(fetchError) {
+    content = <ErrorMessage text={fetchError} />
+  }
+
+  if (fetchedPosts) {
+    content = <BlogPosts posts={fetchedPosts} />
+  } 
+  
+  if (isFetching) {
+    content = <p>Loading blog posts...</p>
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main>
+      <img src={dataFetching} alt="Data Fetching Illustration" />
+      {content}
+    </main>
   )
 }
 
